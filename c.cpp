9a30d2dc93e56c2b5161f293781cd2fedfd3ebba
depgit <depcoder@gmail.com>
2026-03-12 21:13:21 +0530

    Pallindrome

---
+++ c.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;  
+
+// condition / control flow statements
+// function or typee 
+// struct or class 
+
+class c{
+    int x;
+    double y;
+};
+
+int main() {
+    
+}
+
