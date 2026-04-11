d10e59e616ae897b7cd6c859156b2582ec923a31
depgit <depcoder@gmail.com>
2026-04-11 14:25:50 +0530

    struct complete

---
+++ 110426/b.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct Deparment {
+    // task
+    // function
+    // methods
+    // nested struct
+    int x;
+    Deparment(int n){
+        x = n;
+    }
+} ;
+
+int main() {
+    // is structure ka use kare mujhe ye batao
+    // lets imageing ki is structure ka 50 object
+    // Deparment d = Deparment{50};
+    // Deparment d = Deparment[50];
+    // Deparment *d = NULL;
+    // d = new (Deparment)(50 * sizeof(Deparment));
+    // nested structure ka use karke ek new obj
+
+    Deparment::nested obj;
+    d.functoin();
+    obj.functoin();
+
+    return 0;
+}
+
+
